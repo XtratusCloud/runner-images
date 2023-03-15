4a35877601ec4ad14c8dab9788f9babae81b07c3
Francisco Nieto <fnieto@ferrovial.com>
2023-03-15 16:05:29 +0100

    re-enabled fixed test

--- images/win/scripts/Installers/Install-AWS.ps1
+++ images/win/scripts/Installers/Install-AWS.ps1
@@ -15,5 +15,4 @@ $env:Path = $env:Path + ";$env:ProgramFiles\Amazon\SessionManagerPlugin\bin"
 # Install AWS SAM CLI
 Install-Binary -Url "https://github.com/awslabs/aws-sam-cli/releases/latest/download/AWS_SAM_CLI_64_PY3.msi" -Name "AWS_SAM_CLI_64_PY3.msi"
 
-## Xtratus: removed test to avoid error
-# Invoke-PesterTests -TestFile "CLI.Tools" -TestName "AWS"
+Invoke-PesterTests -TestFile "CLI.Tools" -TestName "AWS"
