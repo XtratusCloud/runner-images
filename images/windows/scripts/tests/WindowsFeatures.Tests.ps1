3a0436722350cd030830cda4a1a069fd734eab84
XtratusXloud <xtratuscrossteam@ferrovial.com>
2025-01-02 15:56:07 +0100

    skip esl pester test

--- images/windows/scripts/tests/WindowsFeatures.Tests.ps1
+++ images/windows/scripts/tests/WindowsFeatures.Tests.ps1
@@ -83,7 +83,7 @@ Describe "Windows Updates" {
     }
 }
 
-Describe "WSL2" {
+Describe "WSL2" -Skip {
     It "WSL status should return zero exit code" {
         "wsl --status" | Should -ReturnZeroExitCode
     }
